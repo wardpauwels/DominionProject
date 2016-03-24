da36942bf5c7a71b35bb64918445072f9e801003
rplasschaert <robert.nl@hotmail.nl>
2016-03-24 08:57:59 +0100

    Update Hand.java

--- Dominion/src/Hand.java
+++ Dominion/src/Hand.java
@@ -1,4 +1,49 @@
 
-public class Hand {
+import java.util.*;
 
+
+public class Hand {
+    public ArrayList<Card> hand = new ArrayList<Card>();
+    
+    
+    public Hand(){
+        
+    }
+    
+    
+     private Card getCardFromDeck(){
+        Card cardFromDeck = new Card();
+        return cardFromDeck;
+    }
+    
+    private void fillHand(){
+        for(int i=0; i < 5; i++){
+            Card newCard = getCardFromDeck();
+            hand.add(newCard);
+        }   
+    }
+     
+    public void addCardToHand(Card card){
+        hand.add(card);
+    }
+    private void clearHand(){
+        int handSize = hand.size();
+        for(int i=0; i < handSize; i++){
+            hand.remove(i);
+        }
+        
+        
+    }
+    public Card getCardOnPos(int i){
+        return hand.get(i);
+    }    
+    
+
+    
+    public void printHand(){
+        for (int i = 0; i < hand.size() ;i ++){
+            System.out.println(getCardOnPos(i).getName());
+            }
+        }
+    
 }

f604e5754d7594bc17dda5d6866fa9bae8950ba8
jensthiel <jns.thiel@gmail.com>
2016-05-18 15:22:28 +0200

    ward webservlet.. jens/robert java code

--- Web/src/BoardServlet.java
+++ Web/src/BoardServlet.java
@@ -5,11 +5,14 @@ import javax.servlet.http.*;
 import org.json.JSONObject;
 
 
+
 //@WebServlet(name = "BoardServlet")
 public class BoardServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     String name1;
     String name2;
+    Game g = new Game();
+    ArrayList<String> playerNames;
 
     public void doGet (HttpServletRequest request,HttpServletResponse response)
             throws ServletException, IOException
@@ -27,11 +30,30 @@ public class BoardServlet extends HttpServlet {
                 names.append("name1", name1);
                 names.append("name2", name2);
                 writer.append(names.toString());
+                initGame();
                 break;
+
         }
     }
 
-    public void initGame(){
+    private void initGame(){
 
+        int amountOfPlayers = countAmountOfPlayers();
+        g.createPlayersList(amountOfPlayers);
+        setNames();
+
+
+    }
+    private int countAmountOfPlayers(){
+        return 2; //TODO input via website/gebruiker ipv 2
+    };
+
+    private void firstTurn(){
+
+    }
+    private void setNames(){
+        for (int i=0;i<playerNames.size();i++){
+            g.allPlayers.get(i).setName(playerNames.get(i));
+        }
     }
 }

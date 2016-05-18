05d989e23aa1dbe94a0db12d979af0315db6deb3
Ward Pauwels <wardpauwels@hotmail.be>
2016-05-18 14:56:55 +0200

    Ajax added

--- Web/src/BoardServlet.java
+++ Web/src/BoardServlet.java
@@ -10,28 +10,33 @@ public class BoardServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     String name1;
     String name2;
+    String operation;
+    Writer writer;
+    JSONObject names;
 
     public void doGet (HttpServletRequest request,HttpServletResponse response)
             throws ServletException, IOException
     {
         response.setContentType("application/json");
-        Writer writer = response.getWriter();
-
-        String operation = request.getParameter("operation");
+        writer = response.getWriter();
+        operation = request.getParameter("operation");
 
         switch (operation){
             case "initialize":
-                name1 = request.getParameter("name1");
-                name2 = request.getParameter("name2");
-                JSONObject names = new JSONObject();
-                names.append("name1", name1);
-                names.append("name2", name2);
+                initGame(request);
+                break;
+            case "getNames":
                 writer.append(names.toString());
                 break;
         }
     }
 
-    public void initGame(){
-
+    public void initGame(HttpServletRequest request) throws IOException {
+        name1 = request.getParameter("name1");
+        name2 = request.getParameter("name2");
+        names = new JSONObject();
+        names.append("name1", name1);
+        names.append("name2", name2);
+        writer.append(names.toString());
     }
 }

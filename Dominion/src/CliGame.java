471b0309f3a1eefe8f2368cd73b9a0db31543665
rplasschaert <robert.nl@hotmail.nl>
2016-05-20 09:24:15 +0200

    cli optimized

--- Dominion/src/CliGame.java
+++ Dominion/src/CliGame.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 /**
  * Created by Robert on 21-4-2016.
@@ -10,24 +9,22 @@ public class CliGame {
 
 
     public Scanner in = new Scanner(System.in);
-    // User input
-    private int intInput;
-    private String stringInput;
+
     //
     private int player = 0;
     private int amountOfPlayers;
     private int amountOfCoins = 0;
-    private boolean finished = false;
     private Game g;
     private int numberOTurn = 1;
 
 
      public CliGame() {
-        g = new Game();
-        newGame();
-        showBoard();
-        firstTurn();
-        while (!finished) // hierin de acties per turn zetten
+         g = new Game();
+         newGame();
+         showBoard();
+         firstTurn();
+         boolean finished = false;
+         while (!finished) // hierin de acties per turn zetten
         {
             showBoard();
             nextTurn();
@@ -39,14 +36,12 @@ public class CliGame {
 
     private void newGame() {
         System.out.println("Geef aantal spelers (2 - 4)");
-        intInput = in.nextInt();
-
-        amountOfPlayers = intInput;
+        amountOfPlayers = in.nextInt();
         g.createPlayersList(amountOfPlayers);
         in.nextLine(); // deze nextline staat hier omdat anders de volgende input niet werkt, geen andere oplossing gevonden
         for (int i = 0; i < amountOfPlayers; i++) {
             System.out.println("Geef username speler " + (i + 1));
-            stringInput = in.nextLine();
+            String stringInput = in.nextLine();
             g.setPlayername(i, stringInput);
         }
     }
@@ -58,20 +53,18 @@ public class CliGame {
     private void firstTurn() {
         g.calculateCoinsOfPlayer(g.allPlayers.get(player));
         amountOfCoins = g.getAmountOfCoinsOfPlayer();
-        String playerName = g.getPlayerName(player);
-        System.out.println("-------------------");
-        System.out.println(playerName + " is aan de beurt");
-        g.printHand(g.allPlayers.get(player));
-        g.printCoins();
-        g.printRemainingActions();
-        actionMenu();
-        endTurn();
+        turn();
     }
 
     private void nextTurn() {
         g.calculateCoinsOfPlayer(g.allPlayers.get(player));
         amountOfCoins = g.getAmountOfCoinsOfPlayer();
         nextPlayer();
+        turn();
+
+    }
+
+    private void turn(){
         String playerName = g.getPlayerName(player);
         System.out.println("-------------------");
         System.out.println(playerName + " is aan de beurt");
@@ -80,7 +73,6 @@ public class CliGame {
         g.printRemainingActions();
         actionMenu();
         endTurn();
-
     }
 
     private void endTurn() {
@@ -99,65 +91,35 @@ public class CliGame {
     }
 
     private void actionMenu() {
-
         playActionCard();
         g.resetAmountOfActions();
         buyCard();
         g.endPhase();
-        //g.printDeck(g.allPlayers.get(player)); //Alleen gebruiken bij het testen
-
-
     }
 
-    private void buyCard() { //TODO: check maken of de kaart die je wilt kopen nog niet op is
+    private void buyCard() {
         int remainingBuys = g.getAmountOfActionsRemaining();
         while (remainingBuys != 0) {
             System.out.println("Welk type kaart wil je kopen? 1. Actie. 2. Treasure 3. Victory 4. Stop");
             int keuze = in.nextInt();
-            int kaartKeuze;
-            int cardCost;
             Card card;
+            int kaartKeuze;
             amountOfCoins = g.getAmountOfCoinsOfPlayer();
-
             switch (keuze) {
                 case 1:
-                    System.out.println("Geef positie van te kopen kaart");
-                    kaartKeuze = in.nextInt() - 1;
+                    kaartKeuze = chooseCard();
                     card = g.getCardFromPosInActionTable(kaartKeuze);
-                    cardCost = card.getCost();
-                    if (cardCost <= amountOfCoins) {
-                        g.buyCard(kaartKeuze, card.getType(), g.allPlayers.get(player));
-                        System.out.println("Kaart " + card.getName() + " gekocht");
-                    } else {
-                        System.out.println("onvoldoende coins, probeer opnieuw " + card.getName() + " is te duur");
-                        buyCard();
-                    }
+                    buyCardCheck(card, kaartKeuze);
                     break;
                 case 2:
-                    System.out.println("Geef positie van te kopen kaart");
-                    kaartKeuze = in.nextInt() - 1;
+                    kaartKeuze = chooseCard();
                     card = g.getCardFromPosInTreasureTable(kaartKeuze);
-                    cardCost = card.getCost();
-                    if (card.getCost() <= amountOfCoins) {
-                        g.buyCard(kaartKeuze, card.getType(), g.allPlayers.get(player));
-                        System.out.println("Kaart " + card.getName() + " gekocht");
-                    } else {
-                        System.out.println("onvoldoende coins, probeer opnieuw");
-                        buyCard();
-                    }
+                    buyCardCheck(card, kaartKeuze);
                     break;
                 case 3:
-                    System.out.println("Geef positie van te kopen kaart");
-                    kaartKeuze = in.nextInt() - 1;
+                    kaartKeuze = chooseCard();
                     card = g.getCardFromPosInVictoryTable(kaartKeuze);
-                    cardCost = card.getCost();
-                    if (card.getCost() <= amountOfCoins) {
-                        g.buyCard(kaartKeuze, card.getType(), g.allPlayers.get(player));
-                        System.out.println("Kaart " + card.getName() + " gekocht");
-                    } else {
-                        System.out.println("onvoldoende coins, probeer opnieuw");
-                        buyCard();
-                    }
+                    buyCardCheck(card, kaartKeuze);
                     break;
                 case 4:
                     break;
@@ -167,7 +129,23 @@ public class CliGame {
         }
     }
 
-       private void playActionCard() {
+    private void buyCardCheck(Card card, int kaartKeuze){
+        if (card.getCost() <= amountOfCoins) {
+            g.buyCard(kaartKeuze, card.getType(), g.allPlayers.get(player));
+            System.out.println("Kaart " + card.getName() + " gekocht");
+        } else {
+            System.out.println("onvoldoende coins, probeer opnieuw");
+            buyCard();
+        }
+    }
+
+    private int chooseCard(){
+        System.out.println("Geef positie van te kopen kaart");
+        return in.nextInt() - 1;
+    }
+
+
+    private void playActionCard() {
         Player activePLayer = g.allPlayers.get(player);
         while (g.getAmountOfActionsRemaining() != 0) {
         System.out.println("Geef positie in hand van te spelen actie kaart (Geef 0 in om te stoppen)");

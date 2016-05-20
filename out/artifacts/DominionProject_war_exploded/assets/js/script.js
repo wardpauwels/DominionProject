f3e3ab079761f327d898243fd8dd1546fd29e992
DylanVk <dylan.Van.kerrebroeck@student.howest.be>
2016-05-20 13:47:48 +0200

    Merge branch 'jens' of https://github.com/wardpauwels/DominionProject into Dylan
    # Conflicts:
    #	Web/assets/js/script.js

--- out/artifacts/DominionProject_war_exploded/assets/js/script.js
+++ out/artifacts/DominionProject_war_exploded/assets/js/script.js
@@ -1,3 +1,19 @@
+function setBoard(){
+    var request = $.ajax({ cache: false,
+        url: "/BoardServlet",
+        type: "GET",
+        data:{ operation: 'getNames'} ,
+        success: function (data) {
+            $('#player_one_name').html(data.name1);
+            $('#player_two_name').html(data.name2);
+        },
+        error: function (data) {
+            console.log(data);
+            alert("ERROR: " + data.status);
+        }
+    });
+}
+
 $(document).ready(function () {
     var messages = ['adventurer', 'bureaucrat', 'cellar', 'chancellor', 'chapel', 'councilroom', 'feast', 'festival', 'gardens', 'laboratory', 'library', 'market', 'militia', 'mine', 'moat', 'moneylender', 'remodel', 'smithy', 'Spy', 'thief', 'throneroom', 'village', 'witch', 'woodcutter', 'workshop'];
     console.log("Loaded!");
@@ -9,25 +25,17 @@ $(document).ready(function () {
     showCards(messageArray);
     disableCopyPaste();
 
-    $("#baraja-el li").mousedown(function(e) {
-        if(e.which === 1) {
-            $(this).appendTo('#playedcards_on_table ul')
-        }
-        else {
-            e.preventDefault()
-        }
+    $("#baraja-el li").click(function(e){
+        e.preventDefault();
+        $(this).appendTo('#playedcards_on_table ul');
     });
-    $('body').on('contextmenu', '#wrapper', function(e){ return false; });
 
-
-    $(".showactioncard").click(function () {
+    $("#actioncards_on_table ul li img").click(function () {
         $actioncardOnTableName = $(this).attr("title");
         var src = "<img src='assets/images/Big%20cards/" + $actioncardOnTableName + ".jpg' title = '" + $actioncardOnTableName + "' alt = '" + $actioncardOnTableName + "'/><br>";
 
         $('#bigCard').html(src).css('visibility', 'visible');
     });
-
-    
     $('#bigCard').click(function () {
         $('#bigCard').css('visibility', 'hidden');
     });
@@ -44,34 +52,17 @@ $(document).ready(function () {
             ammountOfPlayers--;
         }
     });
-    $('#startGame').click(function(){
-        var request = $.ajax({ cache: false,
-                url: "/BoardServlet",
-                type: "GET",
-                data:{ operation: 'initialize',
-                    name1: $('#player1').val(),
-                    name2: $('#player2').val(),
-                    name3: $('#player3').val(),
-                    name4: $('#player4').val()
-                }
-        });
-        request.done(function (data) {
-            var obj = JSON.parse(data);
-            console.log("nameP1: "+obj.nameP1+" nameP2: "+obj.nameP2);
-        });
-        request.fail(function (jqXHR, textStatus) {
-            alert(jqXHR.status + ' ' + textStatus);
-        });
-    });
+
 });
 $('#baraja-el li').click(function(){
+    console.log("kaart spelen werkt");
     var request = $.ajax({ cache: false,
         url: "/BoardServlet",
         type: "GET",
-        data:{ operation: 'playCard',
+        data:{ action: 'playCard',
             positionInHand: $('#baraja-el li').index(this)
-            
-            
+
+
         }
         ,
         success: function (data) {
@@ -83,20 +74,60 @@ $('#baraja-el li').click(function(){
             alert("ERROR: " + data.status);
         }
     });
-    
-});
 
+});
+$('#startGame').click(function(){
+    console.log("init werkt");
+    var request = $.ajax({ cache: false,
+        url: "/BoardServlet",
+        dataType: "text",
+        method: "GET",
+        data:{ action: 'init',
+            name1: $('#player1').val(),
+            name2: $('#player2').val(),
+            name3: $('#player3').val(),
+            name4: $('#player4').val()
+        }
+    });
+    request.done(function (data) {
+        cardNames = JSON.parse(data.cards);
+        alert(cardNames);
+    });
+    request.fail(function (jqXHR, textStatus) {
+        console.log("nie gelukt");
+        alert(jqXHR.status + ' ' + textStatus);
+    });
+});
 
 
 function showCards(array) {
     for (var i = 0; i < array.length; i++) {
-        var html = '<li> <p class="counteronactioncards">0</p>';
+        var html = '<li><p class="counteronactioncards">0</p>';
         var src = 'assets/images/Small%20Cards/' + array[i] + '.jpg';
-        html += '<img class="showActionCard" alt="' + array[i] + '"  title="' + array[i] + '" src="' + src + '"/>';
-        html += '<img alt="buyactioncard" title="buyactioncard" src="assets/images/buybutton.png" class="buyActionCard"/></li>';
+        html += '<img alt="' + array[i] + '"  title="' + array[i] + '" src="' + src + '" />';
+        html += '<img alt="buyactioncard" title="buyactioncard" src="assets/images/buybutton.png" class="buyActionCard">'
+        html += '</li>';
         $(".actioncards_on_table_print").append(html);
     }
 }
+
+
+
+
+
+function importPlayerCards(array) {
+    for (var i = 0; i < array.length; i++) {
+        var html = '<li>';
+        var src = 'assets/images/Big%20cards/' + array[i] + '.jpg';
+        html += '<img alt="' + array[i] + '"  title="' + array[i] + '" src="' + src + '" />';
+        html += '</li>';
+        $("#baraja-el").append(html);
+    }
+}
+
+
+
+
 
 
 function getMessage(messages) {
@@ -125,24 +156,6 @@ function disableCopyPaste(){
         e.preventDefault(); return false;
     });
 }
-
-//var allCardsInHand = document.getElementById("baraja-el").children;
-//var zindexArray = [];
-//for (var i = 0; allCardsInHand.length > i; i++) {
-//    zindexArray[i] = allCardsInHand.length - i + 1000 - 1;
-//    console.log(allCardsInHand[i]);
-//    allCardsInHand[i].addEventListener("wheel", function (e) {
-//        this.id=zindexArray[i.toString()];
-//        if (e.wheelDelta < 0) {
-//            console.log(this);
-//            this.style.zIndex = zindexArray[i];
-//            console.log("down");
-//        } else {
-//            this.style.zIndex = 1000000000;
-//            console.log("up");
-//        }
-//    })
-//}
 
 
 document.getElementById('baraja-el').addEventListener("wheel", function (e) {
